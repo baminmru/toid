a7de647adc1aa12287b8fa105b1d4e07033cd20d
baminmru <bami@nm.ru>
2017-01-23 11:53:22 +0300

    add db scripts

---
+++ ORACLE/rawimage.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE rawimage (
+  rawimageid char(38) NOT NULL,
+  changestamp DATE   DEFAULT (sysdate),
+  computername varchar2(64) DEFAULT NULL,
+  fname varchar(255) DEFAULT NULL,
+  filetype varchar2(40) DEFAULT NULL,
+  link1part varchar2(64) DEFAULT NULL,
+  link1id char(38) DEFAULT NULL,
+  link2part varchar2(64) DEFAULT NULL,
+  link2id char(38) DEFAULT NULL,
+  image blob,
+  oper char(38) DEFAULT NULL,
+  PRIMARY KEY (rawimageid)
+) 
